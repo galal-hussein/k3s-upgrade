4de624a4f571303a0520c9be1e7ef138e411094a
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2020-02-20 20:47:30 +0200

    Add wait for master logic

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,15 @@ FROM alpine:3.10
 
 ARG ARCH=
 ARG DRONE_TAG=
+ARG KUBECTL_VERSION=
 
 ENV K3S_RELEASE https://github.com/rancher/k3s/releases/download/${DRONE_TAG}/k3s${ARCH}
 ENV K3S_RELEASE_CHECKSUM https://github.com/rancher/k3s/releases/download/${DRONE_TAG}/sha256sum${ARCH}.txt
 
 RUN apk add -U jq curl
-RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kubectl \
-    && chmod +x ./kubectl \
-    &&  mv ./kubectl /bin/kubectl
+#RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/${ARCH}/kubectl \
+#    && chmod +x ./kubectl \
+#    &&  mv ./kubectl /bin/kubectl
 RUN curl -L -o /opt/k3s ${K3S_RELEASE}
 RUN chmod +x /opt/k3s
 COPY scripts/upgrade.sh /bin/upgrade.sh
--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -65,15 +65,16 @@ check_hash(){
 }
 
 prepare() {
+  KUBECTL_BIN="/opt/k3s kubectl"
   MASTER_PLAN=${1}
   if [ -z "$MASTER_PLAN" ]; then
     fatal "Master Plan name is not passed to the prepare step. Exiting"
   fi
   NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
   # make sure master plan does exist
-  kubectl get plan $MASTER_PLAN -n $NAMESPACE &>/dev/null || fatal "master plan $MASTER_PLAN doesn't exist"
+  ${KUBECTL_BIN} get plan $MASTER_PLAN -n $NAMESPACE &>/dev/null || fatal "master plan $MASTER_PLAN doesn't exist"
   while true; do
-    NUM_NODES=$(kubectl get plan $MASTER_PLAN -n $NAMESPACE -o json | jq '.status.applying | length')
+    NUM_NODES=$(${KUBECTL_BIN} get plan $MASTER_PLAN -n $NAMESPACE -o json | jq '.status.applying | length')
     if [ "$NUM_NODES" == "0" ]; then
       break
     fi
